e6826d743d3c0726101536df2df86718dc6cf38e
lees2541 <suhyeonglee@naver.com>
2026-03-30 21:21:35 +0900

    [feat] 인게임 사거리 표시(임시)

--- Bismuth/Assets/Scripts/이수형/Unit/Combat/UnitAttackSensor.cs
+++ Bismuth/Assets/Scripts/이수형/Unit/Combat/UnitAttackSensor.cs
@@ -9,6 +9,8 @@ public class UnitAttackSensor : MonoBehaviour
     [SerializeField] private CircleCollider2D sensorCollider;
     [SerializeField] private LayerMask monsterLayerMask;
     [SerializeField] private UnitStat unitStat;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private AttackRangeVisualizer rangeVisualizer;
 
     private readonly List<MonsterController> monstersInRange = new();
 
@@ -24,12 +26,30 @@ public class UnitAttackSensor : MonoBehaviour
 
     private void Awake()
     {
+        
         CacheReferences();
+        AddComponent();
+
+        
+        
         ConfigureCollider();
         TrySetDefaultMonsterLayer();
         SyncRadiusFromUnitStat();
+
+        if (rangeVisualizer != null)
+            rangeVisualizer.Show();
     }
 
+    private void AddComponent()
+    {
+        lineRenderer = this.gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.sortingOrder = 10;
+
+        this.gameObject.AddComponent<AttackRangeVisualizer>();
+
+        
+    }
     private void CacheReferences()
     {
         this.gameObject.layer = LayerMask.NameToLayer("Default");
@@ -39,6 +59,17 @@ public class UnitAttackSensor : MonoBehaviour
 
         if (unitStat == null)
             unitStat = GetComponentInParent<UnitStat>();
+
+        if (lineRenderer == null)
+            lineRenderer = GetComponent<LineRenderer>();
+
+        if (rangeVisualizer == null)
+            rangeVisualizer = GetComponent<AttackRangeVisualizer>();
+
+
+
+
+
     }
 
     public void SyncRadiusFromUnitStat()
@@ -50,6 +81,8 @@ public class UnitAttackSensor : MonoBehaviour
 
         float radius = Mathf.Max(0.01f, unitStat.Range);
         sensorCollider.radius = radius;
+
+        rangeVisualizer.Init(sensorCollider, lineRenderer);
     }
 
     private void ConfigureCollider()
