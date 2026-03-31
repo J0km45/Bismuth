c711f15c81bf2f17e97ec9c4393d2a29de92b9f0
Hooni0_0 <startrack0515@gmail.com>
2026-04-01 02:45:48 +0900

    [refactor] 유닛 기본 공격력, 현재 공격력 분리

--- Bismuth/Assets/Scripts/박승훈/Unit/UnitStat.cs
+++ Bismuth/Assets/Scripts/박승훈/Unit/UnitStat.cs
@@ -9,7 +9,8 @@ public class UnitStat : MonoBehaviour
     public int Level;
     public int SummonTime;
     public string Name;
-    public float AttackPower;
+    public float BaseAttackPower;
+    public float CurrentAttackPower;
     public float AttackSpeed;
     public float CritChance;
     public float Range;
