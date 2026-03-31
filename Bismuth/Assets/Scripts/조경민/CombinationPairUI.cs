1601d8706bd6350899871c759e7d5dd6586757ff
Jo Kyeongmin <skykm0405@naver.com>
2026-03-31 17:42:14 +0900

    [update] 합성소 스크롤뷰 수정
    크기도 수정됨

--- Bismuth/Assets/Scripts/조경민/CombinationPairUI.cs
+++ Bismuth/Assets/Scripts/조경민/CombinationPairUI.cs
@@ -12,9 +12,8 @@ public class CombinationPairUI : MonoBehaviour, ICombinationUI
     [Header("━━━━ 조합 대상 이미지 ━━━━")]
     [SerializeField] private Image _resultIcon;
 
-    private CombineData _data;
-    private SummonManager _summonManager;
-    private SummonUnit _summonUnit;
+    private CombineManager _combineManager;
+    private int _index;
 
     private Button _button;
 
@@ -24,18 +23,17 @@ public class CombinationPairUI : MonoBehaviour, ICombinationUI
         _button.onClick.AddListener(OnClickCombine);
     }
 
-    public void Init(SummonManager summonManager, SummonUnit summonUnit)
+    public void Init(CombineManager combineManager, int index)
     {
-        _summonManager = summonManager;
-        _summonUnit = summonUnit;
+        _combineManager = combineManager;
+        _index = index;
     }
 
-    public void SetData(CombineData data, bool canCombine)
+    public void SetData(List<int> sourceIds, int resultId, bool canCombine)
     {
-        _data = data;
-        _sourceIcon1.sprite =  GetIcon(data.SourceUnit[0]);
-        _sourceIcon2.sprite = GetIcon(data.SourceUnit[1]);
-        _resultIcon.sprite = GetIcon(data.ResultUnit);
+        _sourceIcon1.sprite = GetIcon(sourceIds[0]);
+        _sourceIcon2.sprite = GetIcon(sourceIds[1]);
+        _resultIcon.sprite = GetIcon(resultId);
 
         _button.interactable = canCombine;
     }
@@ -47,62 +45,8 @@ public class CombinationPairUI : MonoBehaviour, ICombinationUI
         return unit.Sprite;
     }
 
-    public void OnClickCombine()
+    private void OnClickCombine()
     {
-        List<TowerUnit> removeUnits = GetRemoveUnits(_data);
-
-        if (removeUnits == null) return;
-
-        for (int i = 0; i < removeUnits.Count; i++)
-        {
-            if (removeUnits[i] != null)
-            {
-                _summonManager.DespawnUnit(removeUnits[i]); // 유닛 제거
-            }
-        }
-
-        _summonManager.SummonCombineUnit(_data); // 조합된 유닛 소환
-    }
-
-    private List<TowerUnit> GetRemoveUnits(CombineData data)
-    {
-        // 최종적으로 제거할 유닛을 담을 리스트
-        List<TowerUnit> result = new List<TowerUnit>();
-        // 현재 보유 유닛 중 조합에 필요한 유닛과 일치하는 유닛을 담을 리스트
-        List<SummonUnit.SummonedTowerRecord> candidates = new List<SummonUnit.SummonedTowerRecord>();
-
-        for (int i = 0; i < _summonUnit.OwnedTowers.Count; i++)
-        {
-            SummonUnit.SummonedTowerRecord record = _summonUnit.OwnedTowers[i];
-
-            if (record == null || record.towerUnit == null) continue;
-
-            // 사용 가능한 유닛만 리스트에 추가
-            candidates.Add(record);
-        }
-
-        // 조합식의 재료 ID를 하나씩 확인
-        foreach (int sourceId in data.SourceUnit)
-        {
-            if (sourceId == 0) continue;
-
-            bool found = false;
-
-            for (int i = 0; i < candidates.Count; i++)
-            {
-                // 조합식의 재료 ID와 일치하면 제거리스트에 담고 후보에서 제거
-                if (candidates[i].Id == sourceId)
-                {
-                    result.Add(candidates[i].towerUnit);
-                    candidates.RemoveAt(i);
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) return null;
-        }
-
-        return result;
+        _combineManager.CombineUnit(_index);
     }
 }
