ffb06b6f56d8db206cdacdb088d31aa8cdd9a828
Hooni0_0 <startrack0515@gmail.com>
2026-04-01 02:45:26 +0900

    [feat] 유닛 판매/강화 기능 추가

--- Bismuth/Assets/Scripts/박승훈/Player/Player UI Controller.cs
+++ Bismuth/Assets/Scripts/박승훈/Player/Player UI Controller.cs
@@ -1,16 +1,21 @@
+using System.Diagnostics;
+using System.Linq;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class PlayerUIController : MonoBehaviour
 {
     [SerializeField] private PlayerDataManager _player;
     [SerializeField] private SummonManager _summonManger;
     [SerializeField] private CombineManager _combineManger;
+    [SerializeField] private UnitEnhanceSO _unitEnhanceSO;
     
-    private readonly int MAX_LEVEL = 5;
+    private readonly int MAX_PLAYER_LEVEL = 5;
+    private readonly int MAX_UNIT_LEVEL = 20;
     private readonly int COMBINE_GOLD = 50;
     private readonly int SUMMON_GOLD = 50;
+    private readonly int[] SELL_GOLD_BY_TIER = { 20, 40, 50, 200 };
     private readonly int[] PLAYER_UPGRADE_GOLD = { 110, 220, 330, 440, 550 };
-    private readonly int[] UNIT_UPGRAGE_GOLD = { 3, 5, 10, 20 };
 
     private void Awake()
         => Init();
@@ -41,21 +46,15 @@ public class PlayerUIController : MonoBehaviour
             return;
         }
 
-        int sellGold = stat.Level * 10;
-        switch (stat.Tier)
-        {
-            case 1: sellGold += 20;
-                break;
-            case 2: sellGold += 40;
-                break;
-            case 3: sellGold += 50;
-                break;
-            case 4: sellGold += 200;
-                break;
-        }
+        int payback = stat.Level * 10;
+
+        int sellGold = SellUnit(payback, stat.Tier);
 
         _player.Gold += sellGold;
     }
+
+    private int SellUnit(int payback, int tier)
+        => SELL_GOLD_BY_TIER[tier-1] + payback;
     
     // 유닛 합성 시 호출
     public void OnUnitCombine(int index)
@@ -83,7 +82,53 @@ public class PlayerUIController : MonoBehaviour
     // 유닛 업그레이드 시 호출
     public void OnUnitUpgrade(GameObject unit)
     {
+        if (unit == null)
+        {
+            DebugTool.Log("게임 오브젝트를 찾을 수 없습니다.", DebugType.Unit, this);
+        }
         
+        UnitStat stat = unit.GetComponent<UnitStat>();
+        
+        int unitID = stat.Id;
+        int unitTier = stat.Tier;
+        int unitLevel = stat.Level;
+
+        if (unitLevel < MAX_UNIT_LEVEL)
+        {
+            DebugTool.Log($"유닛 레벨 {unitLevel} : 이미 최고 레벨 입니다.", DebugType.Unit, this);
+        }
+
+        int gold; 
+        switch (unitTier)
+        {
+            case 1:
+                gold = unitLevel * 3 + 15;
+                break;
+            case 2:
+                gold = unitLevel * 5 + 30;
+                break;
+            case 3:
+                gold = unitLevel * 10 + 50;
+                break;
+            case 4:
+                gold =  unitLevel * 20 + 100;
+                break;
+            default:
+                gold = 10000;
+                break;
+        }
+
+        if (_player.Gold < gold)
+        {
+            NotEnoughGold();
+            return;
+        }
+        
+        float UpgradeRatio = _unitEnhanceSO.UnitEnhanceDatas[unitID - 10001].EnhanceValue;
+
+        _player.Gold -= gold;
+        stat.CurrentAttackPower = stat.BaseAttackPower + (stat.BaseAttackPower * UpgradeRatio) * (unitLevel - 1);
+        DebugTool.Log($"유닛 강화 성공! [유닛 레벨 : {unitLevel} | 소모 골드 : {gold}", DebugType.Unit, this);
     }
     
     // 플레이어 레벨 업그레이드 시 호출
@@ -93,9 +138,10 @@ public class PlayerUIController : MonoBehaviour
         CompareGoldLevel(level);
     }
 
+    // 플레이어 레벨과 골드 비교
     private void CompareGoldLevel(int level)
     {
-        if (level == MAX_LEVEL)
+        if (level == MAX_PLAYER_LEVEL)
         {
             DebugTool.Log("이미 최대 레벨 입니다.", DebugType.Game, this);
             return;
@@ -113,7 +159,8 @@ public class PlayerUIController : MonoBehaviour
         
         DebugTool.Log($"골드 : {gold}, 레벨 : {level}", DebugType.Game, this);
     }
-
+    
+    // 유닛 소환 시 호출
     public void OnSummonUnit()
     {
         if (_player.Gold < SUMMON_GOLD)
