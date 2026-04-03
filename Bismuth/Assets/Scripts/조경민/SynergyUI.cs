a1b2fca863fd28cf383fba07f5dd6155bc40b987
Jo Kyeongmin <skykm0405@naver.com>
2026-04-03 16:29:52 +0900

    [update]시너지 스크롤뷰 수정
    시너지 스크롤뷰 수정
    프리팹 수정됨

--- Bismuth/Assets/Scripts/조경민/SynergyUI.cs
+++ Bismuth/Assets/Scripts/조경민/SynergyUI.cs
@@ -5,22 +5,18 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 // 시너지 프리팹에 들어갈 스크립트
-public class SynergyUI : MonoBehaviour, IPointerClickHandler
+public class SynergyUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("━━━━ 헤더 ━━━━")]
     [SerializeField] private Image _icon; // 시너지 이름 앞 네모칸
     [SerializeField] private Sprite activeSprite; // 네모칸에 들어갈 이미지(활성화)
     [SerializeField] private Sprite inactiveSprite; // 네모칸에 들어갈 이미지(비활성화)
-    [SerializeField] private TMP_Text _hNameText; // 헤더에 들어갈 시너지 이름 텍스트
+    [SerializeField] private TMP_Text _NameText; // 시너지 이름 텍스트
     [SerializeField] private TMP_Text _countText; // 시너지 수 (현재 시너지 수/가능한 최대 시너지 수)
-
-    [Header("━━━━ 설명 ━━━━")]
-    [SerializeField] private GameObject _descriptionPanel; // 마우스 올리면 켜질 설명 패널
-    [SerializeField] private TMP_Text _dNameText; // 설명칸에 들어갈 시너지 이름 텍스트
-    [SerializeField] private TMP_Text _descriptionText; // 시너지 설명
 
     private SynergyData _data;
     private int _count;
+    private SynergyScrollViewUI _scrollView;
 
     private void OnEnable()
     {
@@ -32,10 +28,13 @@ public class SynergyUI : MonoBehaviour, IPointerClickHandler
         LocalizationManager.Instance.OnLocalizationLoaded -= RefreshText;
     }
 
-    public void SetData(int synergyId, int count, SynergySO synergySO)
+    public void SetData(int synergyId, int count, SynergySO synergySO, SynergyScrollViewUI scrollView)
     {
         _data = GetSynergyData(synergyId, synergySO);
         _count = count;
+        _scrollView = scrollView;
+
+        if (_data == null) return;
 
         // 최대 시너지 수
         int maxCount = 0;
@@ -60,11 +59,7 @@ public class SynergyUI : MonoBehaviour, IPointerClickHandler
     private void RefreshText()
     {
         // 시너지 이름
-        _hNameText.text = LocalizationManager.Instance.Get(_data.SynergyName);
-        _dNameText.text = LocalizationManager.Instance.Get(_data.SynergyName);
-
-        // 시너지 설명
-        _descriptionText.text = GetDescriptionText(_data, _count);
+        _NameText.text = LocalizationManager.Instance.Get(_data.SynergyName);
     }
 
     private SynergyData GetSynergyData(int synergyId, SynergySO synergySO)
@@ -77,20 +72,20 @@ public class SynergyUI : MonoBehaviour, IPointerClickHandler
         return null;
     }
 
-    private string GetDescriptionText(SynergyData data, int count)
+    public string GetDescriptionText()
     {
-        if (data.Levels == null || data.Levels.Count == 0)
+        if (_data.Levels == null || _data.Levels.Count == 0)
         {
             return "";
         }
 
         SynergyLevelData currentLevel = null;
 
-        for (int i = 0; i < data.Levels.Count; i++)
+        for (int i = 0; i < _data.Levels.Count; i++)
         {
-            if (count >= data.Levels[i].ActiveCount)
+            if (_count >= _data.Levels[i].ActiveCount)
             {
-                currentLevel = data.Levels[i];
+                currentLevel = _data.Levels[i];
             }
         }
 
@@ -100,13 +95,24 @@ public class SynergyUI : MonoBehaviour, IPointerClickHandler
             return "---";
         }
 
-        string key = $"{data.SynergyName}_DESC";
+        string key = $"{_data.SynergyName}_DESC";
         return LocalizationManager.Instance.Get(key, currentLevel.EffectValues.Cast<object>().ToArray());
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+    public string GetSynergyName()
     {
-        bool isDescriptionOpened = _descriptionPanel.activeSelf;
-        _descriptionPanel.SetActive(!isDescriptionOpened);
+        if (_data == null) return "";
+
+        return LocalizationManager.Instance.Get(_data.SynergyName);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _scrollView.ShowDescription(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _scrollView.CloseDescription();
     }
 }
