751dfc81c4859bf78ddb02d56f487f1013534f5f
Hooni0_0 <startrack0515@gmail.com>
2026-04-05 22:19:03 +0900

    [fix] 중복 보유한 유닛 삭제 시 해당 유닛의 시너지의 카운트가 내려가서 현상 해결

--- Bismuth/Assets/Scripts/박승훈/Synergy Manager.cs
+++ Bismuth/Assets/Scripts/박승훈/Synergy Manager.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Text;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,15 +9,23 @@ using UnityEngine.InputSystem;
 
 public class SynergyManager : MonoBehaviour
 {
+    [SerializeField] private SummonUnit _summonUnit;
+    
     public UnityEvent<UnitStat> OnUnitCreated;
     public UnityEvent<UnitStat> OnUnitRemoved;
     public event Action<Dictionary<int, List<int>>> OnSynergyChanged;
 
+    // key = 시너지 ID, value = 해당 시너지를 보유한 유닛의 개수
     private Dictionary<int, List<int>> synergiesDict = new();
 
     [SerializeField] private int _unitCount = 0;
 
     [SerializeField] private bool _log = true;
+
+    private void Awake()
+    {
+        _summonUnit = GetComponent<SummonUnit>();
+    }
 
     private void OnEnable()
     {
@@ -43,19 +52,23 @@ public class SynergyManager : MonoBehaviour
             if (stat.SynergIDs[i] == 0)
                 break;
             
+            // 딕셔너리에 같은 시너지 ID가 없을 경우
             if (!synergiesDict.ContainsKey(stat.SynergIDs[i]))
             {
                 List<int> list = new();
                 list.Add(stat.Id);
+                // 해당 시너지를 보유한 유닛 추가
                 synergiesDict.Add(stat.SynergIDs[i], list);
             }
-            
+
+            // 딕셔너리에 같은 시너지 ID가 있을 경우 해당 유닛이 이미 추가되었는 지 중복 판단
             else
             {
+                // 이미 있을 경우 넘기기
                 if (synergiesDict[stat.SynergIDs[i]].Contains(stat.Id))
-                    continue;
-                else 
-                    synergiesDict[stat.SynergIDs[i]].Add(stat.Id);
+                    break;
+                // 없을 경우 추가               
+                synergiesDict[stat.SynergIDs[i]].Add(stat.Id);
             }
         }
 
@@ -65,17 +78,53 @@ public class SynergyManager : MonoBehaviour
 
     public void RemoveSynergy(UnitStat stat)
     {
+        StringBuilder log = new();
+        log.AppendLine("[시너지 제거 시작]");
+        int idDuplication = 0;
+        int id = stat.Id;
         _unitCount--;
+
+        log.AppendLine($"제거 대상 ID : {stat.Id}, 소유 타워 개수 : {_summonUnit.OwnedTowers.Count}");
+        for (int i = 0; i < _summonUnit.OwnedTowers.Count; i++)
+        {
+            log.Append($"탐색 ID : {_summonUnit.OwnedTowers[i].Id}");
+            if (_summonUnit.OwnedTowers[i].Id == id)
+            {
+                idDuplication++;
+            }
+        }
+
+        log.AppendLine($"중첩 개수 : {idDuplication}");
+
+        if (idDuplication >= 2)
+        {
+            log.AppendLine($"중복 개수 : {idDuplication} 시너지 제거 불필요");
+            DebugTool.Log(log.ToString(), DebugType.Synergy, this);
+            return;
+        }
+        
+        log.AppendLine($"중복 개수 : {idDuplication} 시너지 제거 필요");
+        DebugTool.Log(log.ToString(), DebugType.Synergy, this);
+        
         for (int i = 0; i < stat.SynergIDs.Length; i++)
         {
             if (stat.SynergIDs[i] == 0)
+            {
+                DebugTool.Log("존재하지 않는 시너지 입니다.", DebugType.Synergy, this);
                 break;
-            
+            }
+
             if (synergiesDict.ContainsKey(stat.SynergIDs[i]))
+            {
+                DebugTool.Log($"시너지 ID : {synergiesDict[stat.SynergIDs[i]].Count} 개 ", DebugType.Synergy, this);
                 synergiesDict[stat.SynergIDs[i]].Remove(stat.Id);
-            
-            if(synergiesDict[stat.SynergIDs[i]].Count == 0)
+            }
+
+            if (synergiesDict[stat.SynergIDs[i]].Count == 0)
+            {
+                DebugTool.Log($"시너지 ID : {stat.SynergIDs[i]} 삭제됨", DebugType.Synergy, this);
                 synergiesDict.Remove(stat.SynergIDs[i]);
+            }
         }
         
         PrintSynergy();
